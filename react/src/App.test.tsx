5d319461c679cc51b4cf5af1a5e1f8bfd845d71f
老村翼 <oimurat@systena.co.jp>
2025-07-16 20:26:42 +0900

    test

--- react/src/App.test.tsx
+++ react/src/App.test.tsx
@@ -11,6 +11,6 @@ describe('サンプルテスト', () => {
         render(<App />);
 
         // 検証
-        expect(screen.getByText('React')).toBeInTheDocument();
+        expect(screen.getByText('React!')).toBeInTheDocument();
     });
 });
